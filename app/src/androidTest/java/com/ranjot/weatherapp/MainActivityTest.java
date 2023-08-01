b974a355084f987df69fbf7c60179c156d4362ef
Ranjot <ranjotdhoofer@gmail.com>
2023-08-01 22:52:09 +0100

    initial poject setup

---
+++ app/src/androidTest/java/com/ranjot/weatherapp/MainActivityTest.java
@@ -0,0 +1,32 @@
+package com.ranjot.weatherapp;
+
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(
+//            MainActivity.class);
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void button_clicks() {
+//        activityRule.getScenario().onActivity(activity -> {
+//            activity.findViewById(R.id.test_button).performClick();
+//        });
+        assertEquals(1 + 2, 3);
+    }
+}
---
+++ app/src/main/java/com/ranjot/weatherapp/MainActivity.java
@@ -0,0 +1,2 @@
+package com.ranjot.weatherapp.ui;public class MainActivity {
+}
